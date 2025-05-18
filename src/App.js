05aae15b2cb3c6ab162074a71ab374e99a3193d6
yahiaAz201 <66499992+yahiaAz201@users.noreply.github.com>
2025-05-19 00:25:25 +0200

    Update App.js
    change branches

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
     <>
       <div className="App">
         {!user ? (
-          <HomePage  />
+          <BranchesPage  />
         ) : (
           <Layout>
             <Sider collapsed={collapsed} className="sidebar">
