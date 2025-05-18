76eadc1d705535a0e178a7862146e519874aaf0d
yahiaAz201 <66499992+yahiaAz201@users.noreply.github.com>
2025-05-19 00:29:56 +0200

    Update App.js
    change home

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
     <>
       <div className="App">
         {!user ? (
-          <BranchesPage  />
+          <HomePage   />
         ) : (
           <Layout>
             <Sider collapsed={collapsed} className="sidebar">
