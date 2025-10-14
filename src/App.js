9d13af5934bc5d7ec2b458e4a5bf3514ef5a50de
yahiaAz201 <66499992+yahiaAz201@users.noreply.github.com>
2025-10-15 00:57:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
     <>
       <div className="App">
         {user ? (
-          <AccountPage  />
+          <HomePage  />
         ) : (
           <Layout>
             <Sider collapsed={collapsed} className="sidebar">
